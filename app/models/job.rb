48818adc489e747e3fa3f8f877c6e41d8e5f4c51
Caio Neves <caio.nevesleite@gmail.com>
2017-11-03 22:21:37 +0000

    Default scope to models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
+    default_scope {order(:start_date)}
     validates :name, :description, :start_date, :end_date, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+    default_scope {order(:name)}
     validates :name, :description, :image, :git_url, presence: true
     mount_uploader :image, ImageUploader 
 end
